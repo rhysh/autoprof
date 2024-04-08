0d884e0294e2670786d5aae8b4fdeba851773d59
Rhys Hiltner <rhys.hiltner@gmail.com>
2024-04-08 10:47:07 -0700

    doc: add README

--- doc.go
+++ doc.go
@@ -1,3 +1,5 @@
 // Package autoprof provides tools for Go programs to assemble runtime
 // profiles of themselves.
+//
+// See overview at https://github.com/rhysh/autoprof/blob/main/README.md
 package autoprof
